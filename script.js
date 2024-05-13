d9a3b4ced2c7f2decb946d304f0f3944f2d083b8
kimbacedo <kimbacedo@gmail.com>
2024-05-13 12:45:42 -0300

    animacao ao scroll

---
+++ script.js
@@ -0,0 +1,22 @@
+function initAnimaScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+
+  if (sections.length) {
+    const windowPorcentagem = window.innerHeight * 0.85;
+
+    sections[0].classList.add("ativo");
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop =
+          section.getBoundingClientRect().top - windowPorcentagem;
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimaScroll();
